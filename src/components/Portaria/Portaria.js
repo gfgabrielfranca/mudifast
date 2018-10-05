9e24c486a468a187bf1b5d6eef7bc9d576e31540
Gabriel da Silva França <franca.gabriel@academico.ifrn.edu.br>
2018-10-05 09:38:27 -0300

    Utilizando react-pdf

--- src/components/Portaria/Portaria.js
+++ src/components/Portaria/Portaria.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './Portaria.css';
+import { Document, Page } from 'react-pdf';
 
 const Portaria = () => {
+
   return (
-    <div className="Portaria"></div>
+    <div className="Portaria">
+      <Document
+        file="somefile.pdf"
+        onLoadSuccess={this.onDocumentLoad}>
+        <Page pageNumber={pageNumber} />
+        </Document>
+    </div>
   );
 }
 
