5b155643c1215f3867ba6745b78dda0899afffa8
Gabriel França <gs.gabrielsilvas@gmail.com>
2018-10-22 11:57:22 -0300

    Construindo login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
 import './Login.css';
 
-import TextField from '@material-ui/core/TextField';
-
 class App extends Component {
   render() {
+    const hour = new Date().getHours()
+    let period = ""
+
+    if ((hour >= 0 && hour < 5) || (hour >= 18 && hour <= 23)) {
+      period = "uma boa noite"
+    }
+
+    else if (hour >= 5 && hour <= 11) {
+      period = "um bom dia"
+    }
+
+    else if (hour >= 12 && hour < 18) {
+      period = "uma boa tarde"
+    }
+
     return (
       <div className="Login">
-        <div>
-          <h1>Login</h1>
-          <form>
-            <TextField
-              id="standard-name"
-              label="Name"
-              margin="normal"/>
+        <form>
+          <h1>Bem-vindo ao Mudifast</h1>
+          <p>Tenha {period}</p>
 
-          </form>
-        </div>
+          <div>
+            <div className="input">
+              <h5>Matrícula</h5>
+              <input type="text"/>
+            </div>
+            <div className="input">
+              <h5>Senha</h5>
+              <input type="text"/>
+            </div>
+
+            <button>Entrar</button>
+          </div>
+        </form>
       </div>
     );
   }
